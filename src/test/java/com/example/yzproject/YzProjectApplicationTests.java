f53e2d1dd34877443ae1bd93c798b1a0079a6e47
yoonezii <jeji3391@gmail.com>
2025-02-12 19:57:26 +0900

    init

---
+++ src/test/java/com/example/yzproject/YzProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yzproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YzProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
